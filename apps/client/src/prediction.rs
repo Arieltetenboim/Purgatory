24c0a0b9ebda100f55468273d70f91112b4e3b4e
Ariel <Arieltetenboim@gmail.com>
2026-09-20 14:30:24 +0300

    Apply M4 rustfmt cleanup

--- apps/client/src/prediction.rs
+++ apps/client/src/prediction.rs
@@ -1104,7 +1104,7 @@ mod tests {
                     damage_immunity_active: false,
                 }),
                 equipment: None,
-                            content_id: None,
+                content_id: None,
             }],
             aoi_debug: None,
         }
