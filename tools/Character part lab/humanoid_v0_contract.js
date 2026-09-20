c9c0548f3c80aa422020dd6aac03e1a5962fa5a7
Ariel <Arieltetenboim@gmail.com>
2026-09-20 21:59:17 +0300

    Add Character Base Template V1 metadata

--- tools/Character part lab/humanoid_v0_contract.js
+++ tools/Character part lab/humanoid_v0_contract.js
@@ -19,5 +19,24 @@ window.PURGATORY_HUMANOID_V0_CONTRACT = {
     'upper_leg_back': { parent: 'pelvis', t: [0.04, -0.04], r: 0.00 },
     'lower_leg_back': { parent: 'upper_leg_back', t: [0.00, -0.20], r: 0.00 },
     'foot_back': { parent: 'lower_leg_back', t: [0.00, -0.18], r: 0.00 },
+  },
+  template_v1: {
+    width: 2048, height: 2048, pixels_per_unit: 512.00, cell_size: 512,
+    cells: [
+      { part: 'head', x: 0, y: 0, w: 512, h: 512, pivot: [256, 384] },
+      { part: 'torso', x: 512, y: 0, w: 512, h: 512, pivot: [256, 384] },
+      { part: 'upper_arm_back', x: 1024, y: 0, w: 512, h: 512, pivot: [256, 128] },
+      { part: 'upper_arm_front', x: 1536, y: 0, w: 512, h: 512, pivot: [256, 128] },
+      { part: 'lower_arm_back', x: 0, y: 512, w: 512, h: 512, pivot: [256, 128] },
+      { part: 'lower_arm_front', x: 512, y: 512, w: 512, h: 512, pivot: [256, 128] },
+      { part: 'hand_back', x: 1024, y: 512, w: 512, h: 512, pivot: [256, 128] },
+      { part: 'hand_front', x: 1536, y: 512, w: 512, h: 512, pivot: [256, 128] },
+      { part: 'upper_leg_back', x: 0, y: 1024, w: 512, h: 512, pivot: [256, 96] },
+      { part: 'upper_leg_front', x: 512, y: 1024, w: 512, h: 512, pivot: [256, 96] },
+      { part: 'lower_leg_back', x: 1024, y: 1024, w: 512, h: 512, pivot: [256, 96] },
+      { part: 'lower_leg_front', x: 1536, y: 1024, w: 512, h: 512, pivot: [256, 96] },
+      { part: 'foot_back', x: 0, y: 1536, w: 512, h: 512, pivot: [160, 128] },
+      { part: 'foot_front', x: 512, y: 1536, w: 512, h: 512, pivot: [160, 128] },
+    ]
   }
 };
